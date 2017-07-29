baa930323a461a5a4add9d1c7e9bb64fd6815e6a
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2017-07-29 19:02:33 +0100

    updated database configuration

--- patientappointment/settings/production.py
+++ patientappointment/settings/production.py
@@ -9,10 +9,9 @@ from .common import *
 # Allow all hosts, so we can run on PaaS's like Heroku
 ALLOWED_HOSTS = ['*']
 
-# Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+DATABASES = {
+    'default': dj_database_url.config()
+}
 # Address of RedisToGo instance
 BROKER_URL = os.environ.get('REDISTOGO_URL')
