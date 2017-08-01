da584d97350fb90283746f564efcef608314e1b7
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2017-08-01 12:39:50 +0100

    changed database to postgresql

--- patientappointment/settings/common.py
+++ patientappointment/settings/common.py
@@ -84,18 +84,18 @@ TEMPLATES = [
     },
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
-
-WSGI_APPLICATION = 'patientappointment.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'patientappointment',
+        'USER': 'exalted',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
