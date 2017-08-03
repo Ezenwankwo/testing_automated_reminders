ad3d7d876ade24adce4ee288d22f864e25537a7c
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2017-08-03 03:03:59 +0100

    chnages to database settings

--- patientappointment/settings/common.py
+++ patientappointment/settings/common.py
@@ -88,17 +88,19 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'patientappointment',
-        'USER': 'exalted',
-        'PASSWORD': 'charity24',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+    #'default': {
+       # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       # 'NAME': 'patientappointment',
+       # 'USER': 'exalted',
+       # 'PASSWORD': 'charity24',
+       # 'HOST': '127.0.0.1',
+       # 'PORT': '5432',
+   # }
+#}
 
+import dj_database_url 
+DATABASES = {'default': dj_database_url.parse('postgres://jqtpelyhqjpccl:b210014b8a52852403e0047040d4e665c6d28401ac83ec7a6afcc9d2ff941dbc@ec2-107-20-186-238.compute-1.amazonaws.com:5432/dafs3oulj27jf7')}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
--- patientappointment/settings/production.py
+++ patientappointment/settings/production.py
@@ -9,9 +9,9 @@ from .common import *
 # Allow all hosts, so we can run on PaaS's like Heroku
 ALLOWED_HOSTS = ['*']
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 
 # Address of RedisToGo instance
