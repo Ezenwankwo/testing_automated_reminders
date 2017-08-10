a13020e9cdabe41ec27f83781efddbabbbeefa4d
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2017-08-10 11:12:09 +0100

    changes debug to false

--- patientappointment/settings/common.py
+++ patientappointment/settings/common.py
@@ -13,7 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'nx7e9w_nq@qbr&jec$5vgm^$z$dl2f*&i(k(e*w4^r9i3lyci6'
@@ -26,7 +26,7 @@ TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
 
 # Address of Redis instance, our Celery broker
 #BROKER_URL = 'redis://localhost:6379/0'
-#BROKER_POOL_LIMIT = 8
+BROKER_POOL_LIMIT = 8
 
 # Reminder time: how early text messages are sent in advance of appointments
 REMINDER_TIME = 30 # minutes
